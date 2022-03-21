e5f3c857ee26df7f991591aa027ac2ce747166dc
victor poutot <v.poutot@gmail.com>
2022-03-21 16:02:20 +0100

    1.4

--- django-web-app/merchex/listings/views.py
+++ django-web-app/merchex/listings/views.py
@@ -5,35 +5,24 @@ from django.shortcuts import render
 from listings.models import Band, Listing
 
 
+
 def hello(request):
     bands = Band.objects.all()
-    return HttpResponse(f"""
-        <h1>Hello Django !</h1>
-        <p>Mes groupes préférés sont :<p>
-        <ul>
-            <li>{bands[0].name}</li>
-            <li>{bands[1].name}</li>
-            <li>{bands[2].name}</li>
-        </ul>
-""")
+    return render(request,
+                  'listings/hello.html',
+                  {'bands': bands})
 
 
 def about(request):
-    return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
+    return render(request, 'listings/about.html')
 
 
 def contact(request):
-    return HttpResponse('<h1>contact</h1> <p>Nous adorons merch !</p>')
+    return render(request, 'listings/contact.html')
 
 
 def listings(request):
     listings = Listing.objects.all()
-    return HttpResponse(f"""
-            <h1>liste !</h1>
-            <p>youpla :<p>
-            <ul>
-                <li>{listings[0].title}</li>
-                <li>{listings[1].title}</li>
-                <li>{listings[2].title}</li>
-            </ul>
-    """)
+    return render(request,
+                  'listings/listings.html',
+                  {'listings': listings})
