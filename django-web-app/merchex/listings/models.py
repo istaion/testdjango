77aae7ebed822e759578c7d326900bbd74cca183
Victor Poutot <v.poutot@gmail.com>
2022-03-24 14:42:32 +0100

    4

--- django-web-app/merchex/listings/models.py
+++ django-web-app/merchex/listings/models.py
@@ -1,9 +1,42 @@
 from django.db import models
-
-
-class Listing(models.Model):
-    title = models.fields.CharField(max_length=100)
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Band(models.Model):
     name = models.fields.CharField(max_length=100)
+    biography = models.fields.CharField(max_length=1000)
+    year_formed = models.fields.IntegerField(
+    validators=[MinValueValidator(1900), MaxValueValidator(2021)]
+    )
+    active = models.fields.BooleanField(default=True)
+    official_homepage = models.fields.URLField(null=True, blank=True)
+
+    class Genre(models.TextChoices):
+        HIP_HOP = 'HH'
+        SYNTH_POP = 'SP'
+        ALTERNATIVE_ROCK = 'AR'
+
+    genre = models.fields.CharField(choices=Genre.choices, max_length=5)
+
+    def __str__ (self):
+        return f'{self.name}'
+
+
+class Listing(models.Model):
+    title = models.fields.CharField(max_length=100)
+    description = models.fields.CharField(max_length=1000)
+    sold = models.fields.BooleanField(default=False)
+    year = models.fields.IntegerField(null=True, blank=True,
+    validators=[MinValueValidator(1000), MaxValueValidator(2021)]
+    )
+    band = models.ForeignKey(Band, null=True, on_delete=models.SET_NULL)
+
+    class Type(models.TextChoices):
+        Records = 'RE'
+        Clothing = 'CL'
+        Posters = 'PO'
+        Miscellaneous = 'MI'
+
+    type = models.fields.CharField(choices=Type.choices, max_length=5)
+
+
