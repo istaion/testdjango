cb6b2247ea52f37b8ea12918f432736e0f5dee4d
Victor Poutot <v.poutot@gmail.com>
2022-03-28 19:17:52 +0200

    fin

--- django-web-app/merchex/listings/views.py
+++ django-web-app/merchex/listings/views.py
@@ -4,6 +4,10 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from listings.models import Band, Listing
 
+from listings.form import BandForm, ContactUsForm
+from django.core.mail import send_mail
+from django.shortcuts import redirect
+
 
 def band_list (request):
     bands = Band.objects.all()
@@ -19,12 +23,76 @@ def band_detail(request, band_id):
           {'band': band})
 
 
+def band_create(request):
+    if request.method == 'POST':
+        form = BandForm(request.POST)
+        if form.is_valid():
+            band = form.save()
+            return redirect('band-detail', band.id)
+    else:
+        form = BandForm()
+    return render(request,
+            'listings/band_create.html',
+            {'form': form})
+
+
+def band_update(request, band_id):
+    band = Band.objects.get(id=band_id)
+
+    if request.method == 'POST':
+        form = BandForm(request.POST, instance=band)
+        if form.is_valid():
+            # mettre à jour le groupe existant dans la base de données
+            form.save()
+            # rediriger vers la page détaillée du groupe que nous venons de mettre à jour
+            return redirect('band-detail', band.id)
+    else:
+        form = BandForm(instance=band)
+
+    return render(request,
+                'listings/band_update.html',
+                {'form': form})
+
+
+def band_delete(request, band_id):
+    band = Band.objects.get(id=band_id)  # nécessaire pour GET et pour POST
+
+    if request.method == 'POST':
+        # supprimer le groupe de la base de données
+        band.delete()
+        # rediriger vers la liste des groupes
+        return redirect('band-list')
+
+    # pas besoin de « else » ici. Si c'est une demande GET, continuez simplement
+
+    return render(request,
+                    'listings/band_delete.html',
+                    {'band': band})
+
 def about (request):
     return render(request, 'listings/about.html')
 
 
-def contact (request):
-    return render(request, 'listings/contact.html')
+def contact(request):
+    if request.method == 'POST':
+        form = ContactUsForm(request.POST)
+        if form.is_valid():
+            send_mail(
+            subject=f'Message from {form.cleaned_data["name"] or "anonyme"} via MerchEx Contact Us form',
+            message=form.cleaned_data['message'],
+            from_email=form.cleaned_data['email'],
+            recipient_list=['admin@merchex.xyz'],
+            )
+            return redirect('email-sent')
+    else:
+        form = ContactUsForm()
+    return render(request,
+              'listings/contact.html',
+              {'form': form})
+
+
+def email_sent(request):
+    return render(request, 'listings/email_sent.html')
 
 
 def listings (request):
